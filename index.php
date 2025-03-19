9296560f9cf9b6277147f48975321282e41b9e7e
NatanTeixeiraVieira <natanteixeiravieira@gmail.com>
2025-03-19 19:55:07 -0300

    Init project

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Gerenciador de events</title>
+</head>
+<body>
+  <h1>Gerenciador de eventos</h1>
+  <?php
+
+    
+  ?>
+</body>
+</html>
+
